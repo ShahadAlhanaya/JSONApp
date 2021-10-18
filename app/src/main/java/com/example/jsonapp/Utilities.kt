f4c2d190c0d8c87a9fdde0a90eb80bbdaf436365
ShahadNasser <Shahad.N.Alhanaya@gmail.com>
2021-10-18 18:59:28 +0300

    changed to companion object

---
+++ app/src/main/java/com/example/jsonapp/Utilities.kt
@@ -0,0 +1,10 @@
+package com.example.jsonapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Utilities {
+    companion object {
+        val TAG = "MainActivity"
+    }
+}
